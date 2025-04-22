3e1a2acb2bc62e1a5c6dc4bb089aa1fc38de61d8
Ahmadbambajabbi <jabbiahmadbamba2022@gmail.com>
2025-04-22 18:57:42 +0000

    small erro

--- app/api/subscriptions/route.ts
+++ app/api/subscriptions/route.ts
@@ -127,7 +127,7 @@ export async function POST(request: NextRequest) {
   } catch (error) {
     console.log({ error });
     return NextResponse.json(
-      { error: "Failed to create subscription" err:error },
+      { error: "Failed to create subscription", err: error },
       { status: 500 }
     );
   }
